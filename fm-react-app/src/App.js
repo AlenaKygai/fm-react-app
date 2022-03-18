bb03208a79939e9dd0043553df578690bff06652
AlenaKygai <alena.kygai@gmail.com>
2022-03-18 22:17:42 +0300

    Add component Aloha

--- fm-react-app/src/App.js
+++ fm-react-app/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Greeting from './components/Greeting';
+import Aloha from './components/Aloha';
 function App() {
+  const user= {
+    fname: 'Elon',
+    sname: 'Musk',
+  }
   return <React.Fragment>
-      <Greeting greetings='Hello' name='Elon Musk'/>
-      <Greeting greetings='Hi' name='Elen' />
-      <Greeting greetings='Merhaba' name='Tim Lee' />
+      <Aloha name={`${user.fname} ${user.sname}`} />
+      <Aloha name='Elen' />
+      <Aloha name='Tim Lee' />
   </React.Fragment>
 }
 export default App;
---
+++ fm-react-app/src/components/Aloha/index.jsx
@@ -0,0 +1,24 @@
+import {Component} from 'react';
+class Aloha extends Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      isHi:true,
+    }
+  }
+  handlerBtn = ()=>{
+    const {isHi} = this.state;
+    this.setState({
+      isHi:!isHi
+    })
+  }
+  render(){
+    const {name} = this.props;
+    const {isHi} = this.state;
+    return <>
+    <h2>{isHi ? 'Hi' : 'Bye'}, {name}!</h2>
+    <button onClick={this.handlerBtn} >Aloha</button>
+    </>;
+  }
+}
+export default Aloha;
