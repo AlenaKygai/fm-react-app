d188d4685ac6add1969e54623ae3e9cab032325a
AlenaKygai <alena.kygai@gmail.com>
2022-03-28 22:30:53 +0300

    Add component SelectedUsersList

--- fm-react-app/src/App.js
+++ fm-react-app/src/App.js
@@ -1,10 +1,52 @@
 import './App.css';
-import React from 'react';
+import React, { Component } from 'react';
 import UsersList from './components/AlohaSection/UsersList';
+import SelectedUsersList from './components/AlohaSection/SelectedUsersList';
 
-function App(){
+const userDB = [
+  {
+    id:1,
+    fname:'Dima'
+  },
+  {
+    id:2,
+    fname:'Elen'
+  },
+  {
+    id:3,
+    fname:'Alena'
+  },
+  {
+    id:4,
+    fname:'Bob'
+  },
+  {
+    id:5,
+    fname:'Chen'
+  },
+  {
+    id:6,
+    fname:'Rob'
+  },
+];
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      users:userDB.map((user)=>({...user, isSelected:false}))
+    }
+  }
+
+  setUserSelector = (newUsers)=>{this.setState({users:newUsers})}
+
+  render(){
+    const {users} = this.state;
   return <>
-    <UsersList />
+    <header> <SelectedUsersList  users = {users} /> </header>
+    <main><UsersList users = {users} setUserSelector = {this.setUserSelector} /></main>
   </>
+  }
 }
 export default App;
--- fm-react-app/src/components/AlohaSection/UsersList/index.jsx
+++ fm-react-app/src/components/AlohaSection/UsersList/index.jsx
@@ -1,55 +1,23 @@
 import React, { Component } from 'react';
 import UserCard from '../UserCard';
 
-const userDB = [
-  {
-    id:1,
-    fname:'Dima'
-  },
-  {
-    id:2,
-    fname:'Elen'
-  },
-  {
-    id:3,
-    fname:'Alena'
-  },
-  {
-    id:4,
-    fname:'Bob'
-  },
-  {
-    id:5,
-    fname:'Chen'
-  },
-  {
-    id:6,
-    fname:'Rob'
-  },
-];
-
 class UsersList extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      users:userDB.map((user)=>({...user, isSelected:false})),
-    }
-  }
-  userSelector = (id) => {
-    const {users} = this.state;
+
+  userSelector = (id)=>{
+    const {users, setUserSelector} = this.props;
     // const newUsers = JSON.parse(JSON.stringify(users));
-    this.setState({
-      users: users.map((user)=>({...user,
+    setUserSelector(
+      users.map((user)=>({...user,
         isSelected: user.id===id? !user.isSelected : user.isSelected
       }))
-    })
+    )
   }
   mapUsers = (user)=>{return ( 
     <UserCard key ={user.id} user={user}
     userSelector={this.userSelector}/>
   )}
   render() {
-    const {users} = this.state;
+    const {users} = this.props;
     return (
       <section>
         <h1>List of Users:</h1>
