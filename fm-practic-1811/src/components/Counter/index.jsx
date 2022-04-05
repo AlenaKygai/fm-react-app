5a56fae81878f16b619816eeff5fb4fa1cea6b87
AlenaKygai <alena.kygai@gmail.com>
2022-04-05 18:21:19 +0300

    Add count + 1

--- fm-practic-1811/src/components/Counter/index.jsx
+++ fm-practic-1811/src/components/Counter/index.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0,
+    }
+  }
+  add = () =>{
+    this.setState({count: this.state.count+1})
+  }
+
   render() {
+    const {count} = this.state;
     return (
       <div>
-        
+          <h2>{count}</h2>
+          <button onClick={this.add}>Add</button>
       </div>
     );
   }
