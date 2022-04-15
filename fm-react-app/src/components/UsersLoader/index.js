6a2169e6b3f0fbdf6db39945c7722e91975f7366
AlenaKygai <alena.kygai@gmail.com>
2022-04-15 22:54:41 +0300

    Add query to url

--- fm-react-app/src/components/UsersLoader/index.js
+++ fm-react-app/src/components/UsersLoader/index.js
@@ -3,19 +3,53 @@ import PropTypes from 'prop-types';
 
 
 class UsersLoader extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      users: [],
+      isError: false,
+      isLoaded: false,
+    }
+  }
+  componentDidMount(){
+    this.setState({
+      isLoaded: true,  
+    })
+    fetch('https://randomuser.me/api/?results=10&page=1&seed=users')
+    .then(response => response.json())
+    .then((data)=>this.setState({
+      users:data.results,
+    }))
+    .catch(()=>this.setState({
+      isError: true,
+    }))
+    .finally(()=>this.setState({
+      isLoaded: false,
+    }))
+  }
+
   render() {
+    const {users,isError,isLoaded} = this.state;
+    if(isError){
+      return  <div> Error</div>
+    }
+    if(isLoaded){
+      return  <div> Loading ... </div>
+    }
     return (
       <div>
-        
+        <h2>Users List</h2>
+        <ul>
+          {users.map((user)=>(
+            <li key={user.login.uuid}>{JSON.stringify(user,null,7)}</li>
+          ))}
+        </ul>
       </div>
-    );
-  }
+    )
 }
 
+}
 
-UsersLoader.propTypes = {
-
-};
-
+UsersLoader.propTypes = {};
 
 export default UsersLoader;
