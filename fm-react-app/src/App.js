2709c273f568c597449cd537c8e15384d12718d7
AlenaKygai <alena.kygai@gmail.com>
2022-04-15 17:30:37 +0300

    Add FlexContainer

--- fm-react-app/src/App.js
+++ fm-react-app/src/App.js
@@ -1,32 +1,26 @@
 import './App.css';
 import React from 'react';
-import Container from './components/Grid/Container';
-import Col from './components/Grid/Col';
-import Row from './components/Grid/Row';
+// import Container from './components/Grid/Container';
+// import Col from './components/Grid/Col';
+// import Row from './components/Grid/Row';
 import Aloha from './components/AlohaSection/Aloha';
 import Calendar from './components/Calendar';
+import FlexContainer from './components/FlexContainer';
 
 
 function App (){
     return <>
-      <Container >
-        <Row>
-          <Col colNum='6'>
-            <Aloha  id={2} name="Elon"/>
-          </Col>
-          <Col colNum='6'>
-            <Aloha  id={2} name="Elen"/>
-          </Col>
-        </Row>
-        <Row>
-          <Col colNum='6'>
-            <Aloha  id={2} name="Elon"/>
-          </Col>
-          <Col colNum='6'>
-            <Aloha  id={2} name="Elen"/>
-          </Col>
-        </Row>
-      </Container>
+      <FlexContainer 
+        jc='space-between' 
+        ai='left' 
+        fd='column' 
+        fw='wrap' 
+        style={{border:'1px red solid'}}>
+      <Aloha id={1} name='Elon' />
+      <Aloha id={2} name='Elon' />
+      <Aloha id={3} name='Elon' />
+      <Aloha id={4} name='Elon' />
+      </FlexContainer>
       <Calendar />
     </>
 }
