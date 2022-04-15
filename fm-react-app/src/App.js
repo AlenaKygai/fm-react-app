379b4d34383b159b4bac90662a0c14abe9571983
AlenaKygai <alena.kygai@gmail.com>
2022-04-15 19:49:47 +0300

    Initial commit UsersLoader

--- fm-react-app/src/App.js
+++ fm-react-app/src/App.js
@@ -1,27 +1,11 @@
 import './App.css';
 import React from 'react';
-// import Container from './components/Grid/Container';
-// import Col from './components/Grid/Col';
-// import Row from './components/Grid/Row';
-import Aloha from './components/AlohaSection/Aloha';
-import Calendar from './components/Calendar';
-import FlexContainer from './components/FlexContainer';
+import UsersLoader from './components/UsersLoader';
 
 
 function App (){
     return <>
-      <FlexContainer 
-        jc='space-between' 
-        ai='left' 
-        fd='column' 
-        fw='wrap' 
-        style={{border:'1px red solid'}}>
-      <Aloha id={1} name='Elon' />
-      <Aloha id={2} name='Elon' />
-      <Aloha id={3} name='Elon' />
-      <Aloha id={4} name='Elon' />
-      </FlexContainer>
-      <Calendar />
+      <UsersLoader />
     </>
 }
 export default App;
