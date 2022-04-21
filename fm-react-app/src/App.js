a7296ec11edfa3391b19c93aacb5f0c29d8b8f5b
AlenaKygai <alena.kygai@gmail.com>
2022-04-21 18:37:49 +0300

    Add 404

--- fm-react-app/src/App.js
+++ fm-react-app/src/App.js
@@ -1,12 +1,38 @@
 import './App.css';
 import React from 'react';
-import WindowSizes from './components/WindowSizes';
+import { BrowserRouter, Routes,Route, Link,useNavigate } from "react-router-dom";
+import Calendar from './components/Calendar';
+import Spinner from './components/Spinner';
+import SighInForm from './components/AlohaSection/SignInForm';
 
-
+const Home = ()=> {return <h1>Home</h1>};
 
 function App (){
     return <>
-      <WindowSizes />
+        <BrowserRouter> 
+        <h1>App title</h1>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="calendar">Calendar</Link></li>
+              <li><Link to="spinner">Spinner</Link></li>
+              <li><Link to="sighinform">SighInForm</Link></li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Home /> } />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/spinner" element={<Spinner />} />
+            <Route path="/sighinform" element={<SighInForm />} />
+            <Route path="*" element={<PageNotFound /> } />
+          </Routes>
+        </BrowserRouter>
     </>
 }
+const PageNotFound = (props)=>{
+  const navigate = useNavigate();
+  setTimeout(()=>{navigate('/',{replace:true})},2000);
+  return <h2>404, replace to home page</h2>
+}
+
 export default App;
