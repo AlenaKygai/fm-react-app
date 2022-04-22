3006dcf3b2fef1f7e59e9a614ee0b60a1aecb908
AlenaKygai <alena.kygai@gmail.com>
2022-04-22 18:37:10 +0300

    Add render props

---
+++ fm-react-app/src/components/DataLoader/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class DataLoader extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      data:[],
+      error:null,
+      isFetching:false,
+    }
+  }
+  load = ()=>{
+    // const{path,format} = this.props
+    const{loadData} = this.props
+    this.setState({isFetching:true})
+    // fetch(path)
+    //   .then((response)=>response[format]())
+    loadData()
+      .then((data)=>this.setState({data:data}))
+      .catch((err)=>this.setState({error:err.message}))
+      .finally(()=>this.setState({isFetching:false}))
+  }
+  componentDidMount(){this.load()}
+
+  render() {
+    const {render} = this.props;
+    return render(this.state);
+  }
+}
+
+export default DataLoader;
--- fm-react-app/src/pages/LoaderPage.js
+++ fm-react-app/src/pages/LoaderPage.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import NameList from '../components/NameList';
-import PhoneList from '../components/PhoneList';
+import DataLoader from '../components/DataLoader';
 
-const LoaderPage = () => {
+function LoaderPage () {
+  const loadUsers = () =>{
+    return fetch('/users.json')
+      .then((respons)=>respons.json())
+  }
+  // const loadPhones = () =>{
+  //   return fetch('/phone.json')
+  //     .then((respons)=>respons.json())
+  // }
+
   return (
     <div>
       <h1>Loadboard</h1>
-      <NameList />
-      <PhoneList />
+      <DataLoader loadData={loadUsers} render={
+        (state)=>{
+          return(      
+          <ol>
+            {state.data.map((user)=>{
+              return <li key={user.id}>{user.name}</li>
+            })}
+          </ol>)
+        }
+      }/>
+      {/* <DataLoader loadData={loadPhones} render/> */}
       <Outlet />
       <Link to="/">Home</Link>
     </div>
