d493236dd94c6861a280d2485f83be28cabe64de
AlenaKygai <alena.kygai@gmail.com>
2022-04-29 19:38:00 +0300

    Add renderprops

--- fm-react-app/src/pages/LoaderPage.js
+++ fm-react-app/src/pages/LoaderPage.js
@@ -7,25 +7,35 @@ function LoaderPage () {
     return fetch('/users.json')
       .then((respons)=>respons.json())
   }
-  // const loadPhones = () =>{
-  //   return fetch('/phone.json')
-  //     .then((respons)=>respons.json())
-  // }
-
+  const loadPhones = () =>{
+    return fetch('/phone.json')
+      .then((respons)=>respons.json())
+  }
+  const renderUsers = (state)=> {
+    return(      
+      <ol>
+        {state.data.map((user)=>(
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ol>)
+  }
+  const renderPhones = (state)=> {
+    return(      
+      <ul>
+        {state.data.map((phone)=>(
+          <li key={phone.id}>{phone.name} {phone.price}</li>
+        ))}
+      </ul>)
+  }
   return (
     <div>
       <h1>Loadboard</h1>
-      <DataLoader loadData={loadUsers} children={
-        (state)=>{
-          return(      
-          <ol>
-            {state.data.map((user)=>{
-              return <li key={user.id}>{user.name}</li>
-            })}
-          </ol>)
-        }
-      }/>
-      {/* <DataLoader loadData={loadPhones} render/> */}
+      <DataLoader loadData={loadUsers}>
+        {renderUsers}
+      </DataLoader>
+      <DataLoader loadData={loadPhones}>
+        {renderPhones}
+      </DataLoader>
       <Outlet />
       <Link to="/">Home</Link>
     </div>
