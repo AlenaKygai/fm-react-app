5f25048cad18009d0ea3cc235ac6c9ecb6d02d7b
AlenaKygai <alena.kygai@gmail.com>
2022-06-16 15:22:13 +0300

    Prepare for yup and formik

--- fm-react-app/src/App.js
+++ fm-react-app/src/App.js
@@ -1,36 +1,13 @@
 import './App.css';
 import React, { Component } from 'react';
-// import Tree from './components/Tree';
-import {UserContext,ThemeContext} from './context';
-import HeaderWithTheme from './components/Header';
-import CONSTANTS from './constants';
-import TreeWithTheme from './components/Tree';
-const {THEMES} = CONSTANTS;
+import SignInForm from './components/AlohaSection/SignInForm';
 
-class App extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      theme:THEMES.LIGHT,
-      user:{
-        id:1,
-        fname:'Elon',
-        sname:'Musk',
-        imgSrc:'https://www.presse-citron.net/app/uploads/2021/04/elonmusk.jpg',
-      }
-    }
-  }
-  setTheme = (theme) => {this.setState({theme:theme})}
-  render(){
-    const {user,theme} = this.state;
-      return <ThemeContext.Provider value={[theme,this.setTheme]}>
-      <UserContext.Provider value={user}>
-        <HeaderWithTheme />
-        <TreeWithTheme />
-      </UserContext.Provider>
-      </ThemeContext.Provider>;
-  }
-
+function App (){
+  return(
+    <>
+    <SignInForm/>
+    </>
+  );
 }
 
 export default App;
--- fm-react-app/src/components/AlohaSection/SignInForm/index.jsx
+++ fm-react-app/src/components/AlohaSection/SignInForm/index.jsx
@@ -2,56 +2,9 @@ import React, { Component } from 'react';
 import cx from 'classnames';
 import styles from './SignInForm.module.css';
 
-const initialValues = {
-  email:'',
-  pwd: '',
-  emailValid:true,
-  pwdValid:true,
-};
-class SignInForm extends Component {
-  constructor(props){
-    super(props);
-    this.state = {...initialValues}
-}
-
-  handleInput = ({target:{name,value}}) => this.setState({
-    [name]: value,
-    [`${name}Valid`]: !value.includes(' ')
-  });
-
-  handleForm = (event)=>{
-    event.preventDefault();
-    event.target.reset();
-    this.setState({...initialValues});
-  }
-  render() {
-
-    const {emailValid,pwdValid} = this.state;
-    const emailCX = cx(styles.input,{
-      [styles.invalid]:!emailValid
-    })
-    const pwdCX = cx(styles.input,{
-      [styles.invalid]:!pwdValid
-    })
-
-    return (
-      <form className={styles.container}
-      onSubmit={this.handleForm}>
-        <h2 className={styles.title}>Sign in form</h2>
-        <input type='email' 
-        name='email' 
-        placeholder='email'
-        onChange={this.handleInput}
-        className={emailCX}/>
-        <input type='password' 
-        name='pwd' 
-        placeholder='password'
-        onChange={this.handleInput}
-        className={pwdCX}/>
-        <input type='submit' 
-        value='Sign in' className={styles.btn}/>
-      </form>
-    );
+class SignInForm extends Component{
+  render(){
+    return
   }
 }
 
