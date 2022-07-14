d63f2c5569aa5569ce0e60f1f323f7705d202dd7
Alena.Kygai <alena.kygai@gmail.com>
2022-07-14 23:13:34 +0300

    Add Theme Switch

--- fm-react-app/src/App.js
+++ fm-react-app/src/App.js
@@ -2,22 +2,27 @@ import './App.css';
 import React, { useState }  from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Homepage from './pages/HomePage';
-import {UserContext} from './context';
+import {UserContext,ThemeContext} from './context';
+import CONSTANTS  from './constants';
+const {THEMES} = CONSTANTS;
+
 
 function App (){
   const [user,setUser] = useState({
     id:1,
     name:'Elon Musk',
   });
+  const themeState = useState(THEMES.LIGHT);
   return(
+    <ThemeContext.Provider value={themeState}>
     <UserContext.Provider value={user}>
     <BrowserRouter>
-      <button>Switch theme</button>
       <Routes>
         <Route path='/' element={<Homepage />}/>
       </Routes>
     </BrowserRouter>
     </UserContext.Provider>
+    </ThemeContext.Provider>
   );
 }
 
--- fm-react-app/src/pages/HomePage.js
+++ fm-react-app/src/pages/HomePage.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import UserProfile from '../components/UserProfile';
+import { ThemeContext } from '../context';
+import CONSTANTS  from '../constants';
+const {THEMES} = CONSTANTS;
+const styleMap = {
+  [THEMES.LIGHT]:{
+    background:'#eee', color:'#333', height:'100vh'
+  },
+  [THEMES.DARK]:{
+    background:'#333', color:'#eee', height:'100vh'
+  }
+ }
 
 const Homepage = ()=>{
+  const [theme, themeState] = useContext(ThemeContext);
+  const handlerTheme = ()=>{
+    themeState(theme===THEMES.LIGHT? THEMES.DARK:THEMES.LIGHT);
+  }
   return (
-    <div>
+    <div style={styleMap[theme]}>
+      <button onClick={handlerTheme}>Switch theme</button>
       <UserProfile />
     </div>
-  )
+  );
 }
 
 export default Homepage;
