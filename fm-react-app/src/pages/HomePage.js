42ead13d2e13eb43b2aa5ccfa52428a6098c8c93
Alena.Kygai <alena.kygai@gmail.com>
2022-07-14 23:45:12 +0300

    Add hook useCallback

--- fm-react-app/src/pages/HomePage.js
+++ fm-react-app/src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React,{useContext} from 'react';
+import React,{useState,useContext,useCallback} from 'react';
 import UserProfile from '../components/UserProfile';
 import { ThemeContext } from '../context';
 import CONSTANTS  from '../constants';
@@ -13,13 +13,26 @@ const styleMap = {
  }
 
 const Homepage = ()=>{
-  const [theme, themeState] = useContext(ThemeContext);
-  const handlerTheme = ()=>{
-    themeState(theme===THEMES.LIGHT? THEMES.DARK:THEMES.LIGHT);
-  }
+  const [value,setValue] = useState(0);
+  const [theme, setTheme] = useContext(ThemeContext);
+
+  const handlerTheme = useCallback(()=>{
+    setTheme(theme===THEMES.LIGHT? THEMES.DARK:THEMES.LIGHT);
+  },[theme, setTheme]);
+
+  const handlerInput = useCallback(
+    ({target:{value}})=>{setValue(Number(value));}
+    ,[])
+  const handlerLog = useCallback(
+    ()=>{ console.log(value); }
+    ,[value]
+  );
   return (
     <div style={styleMap[theme]}>
+      <h2>Value: {value}</h2>
+      <input type='number' value={value} onChange={handlerInput}/>
       <button onClick={handlerTheme}>Switch theme</button>
+      <button onClick={handlerLog}>Log value</button>
       <UserProfile />
     </div>
   );
