1944f6c6e9d3fbb295cebba601e9dd998fc2062a
Alena.Kygai <alena.kygai@gmail.com>
2022-07-16 22:37:33 +0300

    Add hook useMemo

--- fm-react-app/src/pages/HomePage.js
+++ fm-react-app/src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React,{useState,useContext,useCallback} from 'react';
+import React,{useState,useContext,useCallback, useMemo} from 'react';
 import UserProfile from '../components/UserProfile';
 import { ThemeContext } from '../context';
 import CONSTANTS  from '../constants';
@@ -11,7 +11,11 @@ const styleMap = {
     background:'#333', color:'#eee', height:'100vh'
   }
  }
-
+function calcValue(n){
+  let res = 0;
+  for(let i=0; i<100000000; i++){ res += i }
+  return n**5;
+}
 const Homepage = ()=>{
   const [value,setValue] = useState(0);
   const [theme, setTheme] = useContext(ThemeContext);
@@ -20,16 +24,14 @@ const Homepage = ()=>{
     setTheme(theme===THEMES.LIGHT? THEMES.DARK:THEMES.LIGHT);
   },[theme, setTheme]);
 
-  const handlerInput = useCallback(
-    ({target:{value}})=>{setValue(Number(value));}
-    ,[])
-  const handlerLog = useCallback(
-    ()=>{ console.log(value); }
-    ,[value]
-  );
+  const handlerInput = useCallback(({target:{value}})=>{setValue(Number(value));},[])
+
+  const handlerLog = useCallback(()=>{ console.log(value); } ,[value]);
+  const showCalcValue = useMemo(()=>calcValue(value), [value])
+
   return (
     <div style={styleMap[theme]}>
-      <h2>Value: {value}</h2>
+      <h2>Value: {showCalcValue}</h2>
       <input type='number' value={value} onChange={handlerInput}/>
       <button onClick={handlerTheme}>Switch theme</button>
       <button onClick={handlerLog}>Log value</button>
