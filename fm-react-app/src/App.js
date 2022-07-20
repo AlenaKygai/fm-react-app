3d38ba59ed00f691af4856cc24bd802572424fcd
Alena.Kygai <alena.kygai@gmail.com>
2022-07-20 22:27:06 +0300

    Add AppContext

--- fm-react-app/src/App.js
+++ fm-react-app/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import React, { useState }  from 'react';
+import React, { useReducer, useState }  from 'react';
 import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 import Homepage from './pages/HomePage';
-import {UserContext,ThemeContext} from './context';
+import {UserContext, ThemeContext, AppContext} from './context';
 import CONSTANTS  from './constants';
 import SignUpForm from './components/forms/SignUpForm';
 import Chat from './components/Chat';
 import NavMenu from './components/NavMenu';
-const {THEMES} = CONSTANTS;
+import reducer from './appReducer';
+const {THEMES, ACTIONS} = CONSTANTS;
 
 
 function App (){
@@ -16,10 +18,14 @@ function App (){
     name:'Elon Musk',
   });
   const themeState = useState(THEMES.LIGHT);
+  const [state,dispatch] = useReducer(reducer,{
+    isMenuOpen:false,
+  })
   return(
     <ThemeContext.Provider value={themeState}>
     <UserContext.Provider value={user}>
     <BrowserRouter>
+      <MenuOpenIcon />
       <NavMenu />
       <Routes>
         <Route path='/' element={<Homepage />}/>
