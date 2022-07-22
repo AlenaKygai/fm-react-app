9e6bd6d1ebe84002c557eccc59f2da972bdd5d94
Alena.Kygai <alena.kygai@gmail.com>
2022-07-22 21:51:19 +0300

    Add keydown

--- fm-react-app/src/App.js
+++ fm-react-app/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useReducer, useState }  from 'react';
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 import Homepage from './pages/HomePage';
 import {UserContext, ThemeContext, AppContext} from './context';
@@ -18,11 +18,11 @@ function App (){
     name:'Elon Musk',
   });
   const themeState = useState(THEMES.LIGHT);
-  const [state, dispatch] = useReducer(reducer,{ isMenuOpen:false });
+  const [state, dispatch] = useReducer(reducer,{ isMenuOpen: false });
   const openMenu = () => dispatch({type: ACTIONS.MENU_OPEN});
   const closeMenu = () => dispatch({type: ACTIONS.MENU_CLOSE});
   return(
-    <AppContext.Provider value={{state, closeMenu}}>
+    <AppContext.Provider value={{state, closeMenu,openMenu}}>
     <ThemeContext.Provider value={themeState}>
     <UserContext.Provider value={user}>
     <BrowserRouter>
--- fm-react-app/src/components/NavMenu/index.js
+++ fm-react-app/src/components/NavMenu/index.js
@@ -1,4 +1,4 @@
-import React,{useContext} from "react";
+import React,{useContext, useEffect, useRef} from "react";
 import cx from 'classnames';
 import { NavLink } from "react-router-dom";
 import CloseIcon from '@mui/icons-material/Close';
@@ -6,22 +6,43 @@ import styles from './NavMenu.module.scss';
 import { AppContext } from "../../context";
 
 
-const NavMenu = () => {
-  const {state:{isMenuOpen},closeMenu} = useContext(AppContext);
+const NavMenu = (props) => {
+  const {state:{isMenuOpen},closeMenu,openMenu} = useContext(AppContext);
   const classNames = cx(styles.container,{
     [styles.open]: isMenuOpen
   });
+  const navRef = useRef(null);
+  useEffect(() => {
+    const handlerClick = ({target}) => {
+      if(isMenuOpen && !navRef.current.contains(target)){
+        closeMenu();
+      }
+    }
+    const downClick = (event) => {
+      if(isMenuOpen){
+        closeMenu();
+      }else{
+        openMenu();
+      }
+    }
+    window.addEventListener('click',handlerClick);
+    window.addEventListener('keydown',downClick);
+    return () => {
+      window.removeEventListener('click',handlerClick);
+      window.removeEventListener('keydown',downClick);
+    }
+  },[isMenuOpen,closeMenu,openMenu])
   return(
-    <nav className={classNames}>
+    <nav className={classNames} ref={navRef}>
       <CloseIcon className={styles.closeBtn} onClick={closeMenu}/>
       <ul className={styles.list}>
         <li>
           <NavLink to='/'>Home</NavLink>
         </li>
-          <li>
+        <li>
           <NavLink to='/signup'>Sign Up</NavLink>
         </li>
-          <li>
+        <li>
           <NavLink to='/chat'>Chat</NavLink>
         </li>
       </ul>
